222f664d40839379496100ac747fd55630ba41bb
PavanReddy21 <pavanmohan2918@gmail.com>
2021-08-20 16:56:31 +0000

    step 7

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,6 @@
-
 const express = require('express');
+const app = new express();
+
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -20,3 +21,34 @@ serviceUrl: api_url,
 
 return naturalLanguageUnderstanding;
 }
+
+// app.use(express.static('client'))
+
+// const cors_app = require('cors');
+// app.use(cors_app());
+
+// app.get("/",(req,res)=>{
+//     res.render('index.html');
+//   });
+
+// app.get("/url/emotion", (req,res) => {
+
+//     return res.send({"happy":"90","sad":"10"});
+// });
+
+// app.get("/url/sentiment", (req,res) => {
+//     return res.send("url sentiment for "+req.query.url);
+// });
+
+// app.get("/text/emotion", (req,res) => {
+//     return res.send({"happy":"10","sad":"90"});
+// });
+
+// app.get("/text/sentiment", (req,res) => {
+//     return res.send("text sentiment for "+req.query.text);
+// });
+
+let server = app.listen(8080, () => {
+    console.log('Listening', server.address().port)
+})
+
